144ddf7e42e9d46387add3a569dadcb0a280e6ae
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 00:23:34 +0200

    comment all de_cipher method

--- de_cipher.rb
+++ de_cipher.rb
@@ -1,23 +1,23 @@
-def encryptor(range, de_encrypted)
-  return de_encrypted.chr unless de_encrypted > range.max
+# def encryptor(range, de_encrypted)
+#   return de_encrypted.chr unless de_encrypted > range.max
 
-  de_encrypted = (de_encrypted - range.max) + (range.min - 1)
-  de_encrypted.chr
-end
+#   de_encrypted = (de_encrypted - range.max) + (range.min - 1)
+#   de_encrypted.chr
+# end
 
-def de_cipher(str, key)
-  str_arr = str.split('')
-  str_digits = []
-  str_arr.map do |current_char|
-    if (65..90).include? current_char.ord
-      de_encrypted = current_char.ord - key % 26
-      str_digits.push(encryptor((65..90), de_encrypted))
-    elsif (97..122).include? current_char.ord
-      de_encrypted = current_char.ord - key % 26
-      str_digits.push(encryptor((97..122), de_encrypted))
-    else
-      str_digits.push(current_char)
-    end
-  end
-  str_digits.join('')
-end
+# def de_cipher(str, key)
+#   str_arr = str.split('')
+#   str_digits = []
+#   str_arr.map do |current_char|
+#     if (65..90).include? current_char.ord
+#       de_encrypted = current_char.ord - key % 26
+#       str_digits.push(encryptor((65..90), de_encrypted))
+#     elsif (97..122).include? current_char.ord
+#       de_encrypted = current_char.ord - key % 26
+#       str_digits.push(encryptor((97..122), de_encrypted))
+#     else
+#       str_digits.push(current_char)
+#     end
+#   end
+#   str_digits.join('')
+# end
