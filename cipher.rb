b43ac2bac0e0adec3afda40360fe8166404bd5cd
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-18 23:35:36 +0200

    finish the cipher

--- cipher.rb
+++ cipher.rb
@@ -1,18 +1,37 @@
-def cipher(str, key)
-  return 'Enter a valid string' unless str.is_a?(String)
-
-  new_arr = []
-  new_str_arr = []
-  str.each_char do |current_char|
-    if current_char == ' '
-      new_arr.push(' ')
-    end
-    new_arr.push(current_char.ord + key)
+def cipher(str, shift)
+  message = str.split("")
+  encoded = [] 
+  message.map do |character| 
+      if is_upper?(character) 
+          shifted = character.ord + shift%26
+          encoded << cipher_shift((65..90), shifted)
+      elsif is_lower?(character)
+          shifted = character.ord + shift%26
+          encoded << cipher_shift((97..122), shifted)
+      else
+          encoded << character
+      end
   end
-  new_arr.each { |current_elt| new_str_arr.push(current_elt.chr) }
-  new_str_arr
+  encoded.join("")
 end
 
-names = 'Emmanuel descholar is cool'
+def is_upper?(char)
+  (65..90).include? char.ord
+end
+
+def is_lower?(char)
+  (97..122).include? char.ord
+end
+
+def cipher_shift(range, shifted)
+  if shifted > range.max 
+      shifted = (shifted - range.max) + (range.min - 1)
+      shifted.chr
+  else 
+      shifted.chr
+  end
+end
+
+names = 'In cryptography, a Caesar cipher, also known as Caesar’s cipher, the shift cipher, Caesar’s code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.'
 cool = cipher(names, 5)
 p cool
