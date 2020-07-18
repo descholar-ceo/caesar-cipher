3ce37a3c08592ba9360986de803f5be6ad6f82ef
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 00:01:51 +0200

    fix rubocop

--- cipher.rb
+++ cipher.rb
@@ -21,7 +21,3 @@ def cipher_shift(range, shifted)
   shifted = (shifted - range.max) + (range.min - 1)
   shifted.chr
 end
-
-names = 'In cryptography, a Caesar cipher, also known as Caesar’s cipher, the shift cipher, Caesar’s code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.'
-cool = cipher(names, 5)
-p cool
