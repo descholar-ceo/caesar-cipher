8261169e3bae402cf1c42edc7a7d8e310fef513a
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-18 22:48:10 +0200

    change the passed string into ciphered alphabets

--- cipher.rb
+++ cipher.rb
@@ -1,3 +1,18 @@
 def cipher(str, key)
-  p "string is #{str} and key is #{key}"
+  return 'Enter a valid string' unless str.is_a?(String)
+
+  new_arr = []
+  new_str_arr = []
+  str.each_char do |current_char|
+    if current_char == ' '
+      new_arr.push(' ')
+    end
+    new_arr.push(current_char.ord + key)
+  end
+  new_arr.each { |current_elt| new_str_arr.push(current_elt.chr) }
+  new_str_arr
 end
+
+names = 'Emmanuel descholar is cool'
+cool = cipher(names, 5)
+p cool
